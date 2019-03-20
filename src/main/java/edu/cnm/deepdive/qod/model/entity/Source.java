00e48e9187eb4bf39e006cfe784cf3d7ad5c30b3
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-03-20 09:52:36 -0600

    Added source and quote repositories

--- src/main/java/edu/cnm/deepdive/qod/model/entity/Source.java
+++ src/main/java/edu/cnm/deepdive/qod/model/entity/Source.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.qod.model.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import edu.cnm.deepdive.qod.view.FlatQuote;
+import edu.cnm.deepdive.qod.view.FlatSource;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,7 +20,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.springframework.lang.NonNull;
 
 @Entity
-public class Source {
+public class Source implements FlatSource {
 
   @Id
   @GeneratedValue(generator = "uuid2") // these next two lines must be used together since
@@ -36,33 +39,23 @@ public class Source {
   @Column(length = 1024, nullable = false, unique = true)
   private String name;
 
+  @JsonSerialize(contentAs = FlatQuote.class)
   @OneToMany(mappedBy = "source", fetch = FetchType.EAGER)
   private List<Quote> quotes = new LinkedList<>();
 
-
   public UUID getId() {
     return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
   }
 
   public Date getCreated() {
     return created;
   }
 
-  public void setCreated(Date created) {
-    this.created = created;
-  }
-
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+  public void setName(String name) { this.name = name; }
 
   public List<Quote> getQuotes() {
     return quotes;
